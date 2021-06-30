903dc53f3feb2558926edb823f33a0f0b26e0663
Anibal <anibal.gatica@mail.udp.cl>
2021-06-30 11:08:15 +0100

    first commit

--- pages/news/index.js
+++ pages/news/index.js
@@ -4,6 +4,9 @@ import { getSortedPostsData } from '../../lib/news';
 import Link from 'next/link';
 import Date from '../../components/date';
 
+// Styles
+import styles from './news.module.css';
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -17,20 +20,22 @@ export async function getStaticProps() {
 export default function News({ allPostsData }) {
   return (
 <Layout>
-  <h1>Music</h1>
-  <ul className={utilStyles.list}>
-    {allPostsData.map(({ id, date, title }) => (
-      <li className={utilStyles.listItem} key={id}>
-        <Link href={`/news/${id}`}>
-          <a>{title}</a>
-        </Link>
-        <br />
-        <small className={utilStyles.lightText}>
-          <Date dateString={date} />
-        </small>
-      </li>
-    ))}
-  </ul>
+  <div className="innerContainer">
+    <h1 className="pageTitle">NEWS</h1>
+    <ul className={utilStyles.list}>
+      {allPostsData.map(({ id, date, title }) => (
+        <li className={utilStyles.listItem} key={id}>
+          <Link href={`/news/${id}`}>
+            <a>{title}</a>
+          </Link>
+          <br />
+          <small className={utilStyles.lightText}>
+            <Date dateString={date} />
+          </small>
+        </li>
+      ))}
+    </ul>
+  </div>
 </Layout>
 )
 }
