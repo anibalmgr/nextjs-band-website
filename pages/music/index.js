7dafbe0ffeded7cb81309e0968cb2e5903500e86
Anibal <anibal.gatica@mail.udp.cl>
2021-07-09 12:48:21 +0100

    music page started

--- pages/music/index.js
+++ pages/music/index.js
@@ -1,11 +1,20 @@
 import Layout, { siteTitle } from '../../components/layout';
+import styles from './music.module.css';
 
 export default function About() {
   return (
     <Layout>
       <div className="innerContainer">
-        <h1>Music</h1>
+        <h1 className={styles.title}>Music</h1>
+        <div className={styles.musicBox}>
+          <img className={styles.musicImg} src="./images/single-cover-1.jpg" alt="Cover of The State of Things" />
+          <div className={styles.musicInfo}>
+            <p>Pacific Oceans</p>
+            <h3>The State of Things</h3>
+            
+          </div>
+        </div>
       </div>
-    </Layout>
+      </Layout>
   )
 }
