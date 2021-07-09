6c927eb413f73b39a574a62847f51e256e9aa8aa
Anibal <anibal.gatica@mail.udp.cl>
2021-07-09 15:57:53 +0100

    music page and button component changes

--- pages/music/index.js
+++ pages/music/index.js
@@ -1,3 +1,4 @@
+import { useMediaQuery } from 'react-responsive';
 
 // Components
 import Layout, { siteTitle } from '../../components/layout';
@@ -8,6 +9,9 @@ import styles from './music.module.css';
 
 
 export default function About() {
+
+  const isMobile = useMediaQuery({query: '(max-width: 500px)'})
+
   return (
     <Layout>
       <div className="innerContainer">
@@ -17,8 +21,8 @@ export default function About() {
           <div className={styles.musicInfo}>
             <h3>Pacific Oceans</h3>
             <h2>The State of Things</h2>
-            <Button link="" text="" />
-            <Button link="" text="" />
+            <Button link={isMobile ? "spotify:track:7Igl0UrESGJcG5b3uumaD7" : "https://open.spotify.com/track/7Igl0UrESGJcG5b3uumaD7?si=6e6b7802652e403a"} text="Listen on Spotify" />
+            <Button link="https://music.apple.com/us/album/the-state-of-things-single/1572364582?uo=4&app=music&at=1001lry3&ct=dashboard" text="Listen on Apple Music" />
             <Button link="" text="" />
           </div>
         </div>
